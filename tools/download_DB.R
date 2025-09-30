114b1a82278fa2edabcaf72aed5417be68237fc2
Harry Gotsall <s1147934@student.hsleiden.nl>
2025-09-30 09:35:44 +0200

    v2 forgot the library call

--- tools/download_DB.R
+++ tools/download_DB.R
@@ -1,20 +1,19 @@
+library(GenomicFeatures)
+
 path <- getwd()
 
 # Build a subfolder path dynamically.
 location <- file.path(path, "Data")
 db_file <- file.path(location, "txdb_hsa.db")
 
-# Create the folder if it doesn't exist.
 if (!dir.exists(location)) {
   dir.create(location, recursive = TRUE)
 }
 
-# Build the full destination path for the file.
 destination <- file.path(location, "Homo_sapiens.GRCh38.115.gtf.gz")
 
 url <- "https://ftp.ensembl.org/pub/current_gtf/homo_sapiens/Homo_sapiens.GRCh38.115.gtf.gz"
 
-# Download the file to the specified location.
 download.file(url, destfile = destination, mode = "wb")
 
 # Check if file is downloaded and if it is downloaded make a db file with it.
