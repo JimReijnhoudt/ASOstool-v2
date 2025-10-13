ee0e60ae77d1a33923859128dbd6d47dce923a77
s1148557 <s1148557@student.hsleiden.nl>
2025-10-13 16:37:57 +0200

    ipynb > R
    the off target identification script is rewritten in R.

--- off_target_script.R
+++ off_target_script.R
@@ -1,9 +1,8 @@
 library(tidyverse)
 library(httr)
-library(readr)
-library(stringr)
-
-# ---------- Functies ----------
+# library(readr)
+# library(stringr)
+library("xlsx")
 
 generate_urls <- function(aso_sequence, mismatch_conditions) {
   base_url <- "https://gggenome.dbcls.jp/hg38"
@@ -32,6 +31,7 @@ generate_urls <- function(aso_sequence, mismatch_conditions) {
   )
   return(urls_df)
 }
+
 
 filter_data <- function(data, criterion) {
   start <- c('NM_', 'chr', 'NR_')
@@ -66,6 +66,7 @@ filter_data <- function(data, criterion) {
   return(df)
 }
 
+
 fetch_protein_expression <- function(protein_name) {
   base_url <- "https://www.proteinatlas.org/api/search_download.php?search="
   columns <- "&columns=g,gd,brain_RNA_amygdala,brain_RNA_basal_ganglia,brain_RNA_cerebellum,brain_RNA_cerebral_cortex,brain_RNA_choroid_plexus,brain_RNA_hippocampal_formation,brain_RNA_hypothalamus,brain_RNA_medulla_oblongata,brain_RNA_midbrain,brain_RNA_pons,brain_RNA_spinal_cord,brain_RNA_thalamus&compress=no&format=tsv"
@@ -82,35 +83,23 @@ fetch_protein_expression <- function(protein_name) {
   }
 }
 
-# ---------- Main ----------
 
 main <- function() {
+  # Define the file name at the top
+  output_file_name <- "20250801_EH_UNC13A_SNP151&41_ASO146_R.xlsx"
+
+  # Define the ASO sequence
+  aso_sequence <- "CACACCATGCACATTCAA"  # Replace with your actual sequence
   
-  aso_sequence <- "CACACCATGCACATTCAA"
-  mismatches_allowed <- 2
+  # Calculate the number of mismatches based on the sequence length
+  sequence_length = nchar(aso_sequence)
+  mismatches_allowed = 2  # int(sequence_length * 0.1)  # 20% of the sequence length
   
-  urls_df <- generate_urls(aso_sequence, c("2/"))
+  mismatch_conditions <- c("2/")
   
-  summary_df <- data.frame(
-    # 'Protein Hit' = character(),
-    # 'Source Sheets' = character(),
-    # 'Equal Signs' = integer(),
-    # 'Total Mismatches' = integer(),
-    # 'Gene description' = character(),
-    # 'Brain RNA - amygdala [nTPM]' = numeric(),
-    # 'Brain RNA - basal ganglia [nTPM]' = numeric(),
-    # 'Brain RNA - cerebellum [nTPM]' = numeric(),
-    # 'Brain RNA - cerebral cortex [nTPM]' = numeric(),
-    # 'Brain RNA - choroid plexus [nTPM]' = numeric(),
-    # 'Brain RNA - hippocampal formation [nTPM]' = numeric(),
-    # 'Brain RNA - hypothalamus [nTPM]' = numeric(),
-    # 'Brain RNA - medulla oblongata [nTPM]' = numeric(),
-    # 'Brain RNA - midbrain [nTPM]' = numeric(),
-    # 'Brain RNA - pons [nTPM]' = numeric(),
-    # 'Brain RNA - spinal cord [nTPM]' = numeric(),
-    # 'Brain RNA - thalamus [nTPM]' = numeric(),
-    # stringsAsFactors = FALSE
-  )
+  urls_df <- generate_urls(aso_sequence, mismatch_conditions)
+  
+  summary_df <- data.frame()
   
   all_df <- data.frame(
     line = character(),
@@ -119,9 +108,8 @@ main <- function() {
     stringsAsFactors = FALSE
   )
   
-  all_protein_df <- data.frame()
+  sheetnames <- c()
   
-  # Loop over alle URLs
   for (i in 1:nrow(urls_df)) {
     url <- urls_df$url[i]
     condition <- urls_df$condition[i]
@@ -139,24 +127,20 @@ main <- function() {
       next
     }
     
-    # Extract protein hits
+
     protein_hits <- str_match(filtered_data_df$line, "NM_\\d+\\.\\d+\\|([^;]+)")[,2]
     protein_hits <- unique(na.omit(protein_hits))
     
     for (protein_hit in protein_hits) {
       expression_data <- fetch_protein_expression(protein_hit)
       if (!is.null(expression_data) && nrow(expression_data) > 0) {
-        
-        # Filter protein_row
+
         protein_row <- filtered_data_df %>% filter(str_detect(line, protein_hit))
         max_equal <- max(protein_row$equal_signs, na.rm = TRUE)
         min_mismatch <- min(protein_row$mismatches, na.rm = TRUE)
-        # filtered_row <- protein_row %>% filter(equal_signs == max_equal & mismatches == min_mismatch) %>% slice(1)
-        
-        # Source Sheet
+
         source_sheet <- paste0(str_replace_all(condition, "\\W+", "_"), "_", str_replace_all(mismatch_condition, "\\W+", "_"), "_mismatch")
         
-        # Maak summary row
         summary_row <- data.frame(
           'Protein Hit' = protein_hit,
           'Source Sheets' = source_sheet,
@@ -165,27 +149,43 @@ main <- function() {
           stringsAsFactors = FALSE
         )
         
-        # # Voeg expression data toe
-        # expr_cols <- c('Gene description','Brain RNA - amygdala [nTPM]','Brain RNA - basal ganglia [nTPM]',
-        #                'Brain RNA - cerebellum [nTPM]','Brain RNA - cerebral cortex [nTPM]',
-        #                'Brain RNA - choroid plexus [nTPM]','Brain RNA - hippocampal formation [nTPM]',
-        #                'Brain RNA - hypothalamus [nTPM]','Brain RNA - medulla oblongata [nTPM]',
-        #                'Brain RNA - midbrain [nTPM]','Brain RNA - pons [nTPM]',
-        #                'Brain RNA - spinal cord [nTPM]','Brain RNA - thalamus [nTPM]')
-        
-        # summary_row <- bind_cols(summary_row, expression_data[1, ])
-        # summary_df <- bind_rows(summary_df, summary_row)
-        # summary_df <- summary_df %>% select(where(~ !all(is.na(.))))
-        
         summary_row <- bind_cols(summary_row, expression_data[1, 2:ncol(expression_data)])
         summary_df <- bind_rows(summary_df, summary_row)
-        
-        # # Voeg toe aan all_protein_df
-        # expression_data$Source_Sheet <- source_sheet
-        # all_protein_df <- bind_rows(all_protein_df, expression_data)
+      }
+      else {
+        print(paste("No expression data found for protein", protein_hit))
       }
     }
+    if (nrow(all_df) == 0) {
+      all_df <- data.frame(
+        line = NULL,
+        equal_signs = NULL,
+        mismatches = NULL,
+        stringsAsFactors = FALSE
+      )
+    }
+    
+    if (str_detect(str_to_lower(condition), "prespliced")) {
+      cond_short <- "_RefSeqCurated_p_d3g2202/"
+    } 
+    else if (str_detect(str_to_lower(condition), "spliced")) {
+      cond_short <- "_RefSeqCurated_s_d3g2202/"
+    } 
+    else {
+      cond_short <- ".p13_d3g2202/"
+    }
+
+    clean_cond_short <- str_replace_all(cond_short, "\\W+", "_")
+    clean_mismatch <- str_replace_all(mismatch_condition, "\\W+", "_")
+
+    sheetnames <- c(sheetnames, paste0(clean_cond_short, "_", clean_mismatch, "_mm"))
+    
   }
+  
+  spliced_df <- all_df[str_detect(str_to_lower(all_df$line), "\\|spliced"), ]
+  prespliced_df <- all_df[str_detect(str_to_lower(all_df$line), "\\|prespliced"), ]
+  other_df <- setdiff(all_df, rbind(spliced_df, prespliced_df))
+  
   new_colnames <- c("Protein Hit", "Source Sheets", "Equal Signs", "Total Mismatches",
                     "Gene description", "Brain RNA - amygdala [nTPM]",
                     "Brain RNA - basal ganglia [nTPM]", "Brain RNA - cerebellum [nTPM]",
@@ -196,7 +196,13 @@ main <- function() {
                     "Brain RNA - thalamus [nTPM]")
 
   colnames(summary_df) <- new_colnames
-  return(list(urls = urls_df, summary = summary_df, df = all_df, prot_df = all_protein_df))
+  
+  write.xlsx(summary_df, file = output_file_name, sheetName="Summary")
+  write.xlsx(spliced_df, file = output_file_name, sheetName=sheetnames[str_detect(sheetnames, "_s_")], append=TRUE)
+  write.xlsx(prespliced_df, file = output_file_name, sheetName=sheetnames[str_detect(sheetnames, "_p_")], append=TRUE)
+  write.xlsx(other_df, file = output_file_name, sheetName=sheetnames[!str_detect(sheetnames, "_(p|s)_")], append=TRUE)
+  
+  return(list(urls = urls_df, summary = summary_df, df = all_df, spliced = spliced_df, prespliced = prespliced_df, other = other_df))
 }
 
 # ---------- Run ----------
@@ -204,4 +210,6 @@ dataframes <- main()
 urls <- dataframes$urls
 summary_df <- dataframes$summary
 df <- dataframes$df
-protein_expression <- dataframes$prot_df
+spliced <- dataframes$spliced
+prespliced <- dataframes$prespliced
+other <- dataframes$other
